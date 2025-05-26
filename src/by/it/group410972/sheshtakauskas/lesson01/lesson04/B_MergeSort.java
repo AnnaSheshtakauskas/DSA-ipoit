d08f21379b8e7c65ddfc7f9b17d49cc58cc301a1
Anna <anna.sheshtakauskas@mail.ru>
2025-05-26 15:05:27 +0300

    Lesson4 B

---
+++ src/by/it/group410972/sheshtakauskas/lesson01/lesson04/B_MergeSort.java
@@ -0,0 +1,85 @@
+package by.it.group410972.sheshtakauskas.lesson01.lesson04;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Реализуйте сортировку слиянием для одномерного массива.
+Сложность алгоритма должна быть не хуже, чем O(n log n)
+
+Первая строка содержит число 1<=n<=10000,
+вторая - массив A[1…n], содержащий натуральные числа, не превосходящие 10E9.
+Необходимо отсортировать полученный массив.
+
+Sample Input:
+5
+2 3 9 2 9
+Sample Output:
+2 2 3 9 9
+*/
+public class B_MergeSort {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream stream = B_MergeSort.class.getResourceAsStream("dataB.txt");
+        B_MergeSort instance = new B_MergeSort();
+        //long startTime = System.currentTimeMillis();
+        int[] result = instance.getMergeSort(stream);
+        //long finishTime = System.currentTimeMillis();
+        for (int index : result) {
+            System.out.print(index + " ");
+        }
+    }
+
+    int[] getMergeSort(InputStream stream) throws FileNotFoundException {
+        Scanner scanner = new Scanner(stream);
+
+        int n = scanner.nextInt();
+
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        mergeSort(a, 0, n - 1);
+
+        return a;
+    }
+    void mergeSort(int[] a, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            mergeSort(a, l, m);
+            mergeSort(a, m + 1, r);
+            merge(a, l, m, r);
+        }
+    }
+    void merge(int[] a, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            left[i] = a[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = a[m + 1 + j];
+        }
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                a[k++] = left[i++];
+            } else {
+                a[k++] = right[j++];
+            }
+        }
+        while (i < n1) {
+            a[k++] = left[i++];
+        }
+        while (j < n2) {
+            a[k++] = right[j++];
+        }
+    }
+}
