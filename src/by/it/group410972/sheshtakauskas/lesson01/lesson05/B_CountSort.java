0a2df4ed793a3eac1cd89a3cdda2003a529e1986
Anna <anna.sheshtakauskas@mail.ru>
2025-05-26 15:21:11 +0300

    Lesson5 B

---
+++ src/by/it/group410972/sheshtakauskas/lesson01/lesson05/B_CountSort.java
@@ -0,0 +1,55 @@
+package by.it.group410972.sheshtakauskas.lesson01.lesson05;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Первая строка содержит число 1<=n<=10000, вторая - n натуральных чисел, не превышающих 10.
+Выведите упорядоченную по неубыванию последовательность этих чисел.
+
+При сортировке реализуйте метод со сложностью O(n)
+
+Пример: https://karussell.wordpress.com/2010/03/01/fast-integer-sorting-algorithm-on/
+Вольный перевод: http://programador.ru/sorting-positive-int-linear-time/
+*/
+
+public class B_CountSort {
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream stream = B_CountSort.class.getResourceAsStream("dataB.txt");
+        B_CountSort instance = new B_CountSort();
+        int[] result = instance.countSort(stream);
+        for (int index : result) {
+            System.out.print(index + " ");
+        }
+    }
+
+    int[] countSort(InputStream stream) throws FileNotFoundException {
+        Scanner scanner = new Scanner(stream);
+
+        int n = scanner.nextInt();
+        int[] points = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            points[i] = scanner.nextInt();
+        }
+
+        int[] freq = new int[11]; // 0..10
+        for (int num : points) {
+            freq[num]++;
+        }
+
+        int[] sorted = new int[n];
+        int index = 0;
+        for (int num = 0; num < freq.length; num++) {
+            for (int count = 0; count < freq[num]; count++) {
+                sorted[index++] = num;
+            }
+        }
+
+        return sorted;
+    }
+
+}
